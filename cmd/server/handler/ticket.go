0aac2bb04a2dc0e48c9465dfbba453521dce7afe
frniz <franco.niz@mercadolibre.com>
2022-10-12 09:29:58 -0300

    token validator

--- cmd/server/handler/ticket.go
+++ cmd/server/handler/ticket.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"desafio-goweb-franconiz/internal/tickets"
+	"desafio-goweb-franconiz/pkg/web"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,6 +24,11 @@ func NewService(s tickets.Service) *Service {
 func (s *Service) GetAll() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		if err := tokenValidator(c); err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, web.InvalidToken))
+			return
+		}
+
 		tickets, err := s.service.GetTotalTickets()
 		if err != nil {
 			c.String(http.StatusInternalServerError, err.Error(), nil)
@@ -33,6 +41,11 @@ func (s *Service) GetAll() gin.HandlerFunc {
 
 func (s *Service) GetTicketsByCountry() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		if err := tokenValidator(c); err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, web.InvalidToken))
+			return
+		}
 
 		destination := c.Param("dest")
 
@@ -49,6 +62,11 @@ func (s *Service) GetTicketsByCountry() gin.HandlerFunc {
 func (s *Service) GetAverageDestination() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		if err := tokenValidator(c); err != nil {
+			c.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, web.InvalidToken))
+			return
+		}
+
 		destination := c.Param("dest")
 
 		avg, err := s.service.GetAverageDestination(destination)
@@ -59,4 +77,14 @@ func (s *Service) GetAverageDestination() gin.HandlerFunc {
 
 		c.JSON(200, avg)
 	}
+}
+
+func tokenValidator(ctx *gin.Context) error {
+	token := ctx.Request.Header.Get("token")
+	if token != os.Getenv("TOKEN") {
+
+		return fmt.Errorf("Error: Token invalido.")
+
+	}
+	return nil
 }
